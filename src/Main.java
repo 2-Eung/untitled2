6c6e2b06c0a43729652b61e1e5cfd0f758e92e1a
2-Eung <eung4256@gmail.com>
2025-06-02 14:36:06 +0900

    switch

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 1;
+        // happy, sad, angry, tired
+        String mood = "happy";
 
-        if (number > 0) {
-            System.out.println("number는 양수입니다.");
-        }
-        else if (number < 0){
-            System.out.println("number는 음수입니다.");
-        }
-        else {
-            System.out.println("number는 0입니다.");
+        switch (mood){
+            case "happy" :
+                System.out.println("나는 행복하다.");
+                break;
+            case "sad" :
+                System.out.println("나는 슬프다.");
+                break;
+            case "angry" :
+                System.out.println("나는 화가난다");
+                break;
+            case "tired" :
+                System.out.println("나는 피곤하다");
+                break;
         }
     }
 }
